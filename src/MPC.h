be59a708ee5a0db78411c96684991b22aebd4be7
tobycoleman <toby@tobycoleman.com>
2017-06-17 15:40:17 +0100

    MPC implemented

--- src/MPC.h
+++ src/MPC.h
@@ -6,11 +6,24 @@
 
 using namespace std;
 
+constexpr double pi();
+double deg2rad(double x);
+double rad2deg(double x);
+
 class MPC {
+ private:
+  double ref_v_;        // Target velocity
+  int delay_;           // Delay timesteps to simulate latency
+
+  double throttle_ = 0; // Last computed throttle
+  double steer_ = 0;    // Last computed steering angle
  public:
-  MPC();
+  MPC(double target_velocity, int delay_steps);
 
   virtual ~MPC();
+  // Variables to store predicted trajectory
+  std::vector<double> x_predict;
+  std::vector<double> y_predict;
 
   // Solve the model given an initial state and polynomial coefficients.
   // Return the first actuatotions.
--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,6 @@
 // for convenience
 using json = nlohmann::json;
 
-// For converting back and forth between radians and degrees.
-constexpr double pi() { return M_PI; }
-double deg2rad(double x) { return x * pi() / 180; }
-double rad2deg(double x) { return x * 180 / pi(); }
-
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
 // else the empty string "" will be returned.
@@ -68,8 +63,9 @@ Eigen::VectorXd polyfit(Eigen::VectorXd xvals, Eigen::VectorXd yvals,
 int main() {
   uWS::Hub h;
 
-  // MPC is initialized here!
-  MPC mpc;
+  // MPC is initialized here
+  MPC mpc(60.0, // Target velocity
+          2);   // Delay steps
 
   h.onMessage([&mpc](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
@@ -98,8 +94,34 @@ int main() {
           * Both are in between [-1, 1].
           *
           */
-          double steer_value;
-          double throttle_value;
+
+          // Transform ideal path to vehicle coords
+          Eigen::VectorXd ptsx_v(ptsx.size());
+          Eigen::VectorXd ptsy_v(ptsy.size());
+          for (int i = 0; i < ptsx.size(); i++) {
+            double x = ptsx[i] - px;
+            double y = ptsy[i] - py;
+            ptsx_v[i] = x * cos(-psi) - y * sin(-psi);
+            ptsy_v[i] = x * sin(-psi) + y * cos(-psi);
+          }
+          // Fit cubic polynomial
+          auto coeffs = polyfit(ptsx_v, ptsy_v, 3);
+
+          // Calculate the cross track error
+          double cte = coeffs[0]; // Intercept term
+          // Calculate the orientation error
+          double epsi = -atan(coeffs[1]); // psi=0 in vehicle frame
+
+          // Set up state in vehicle coords
+          Eigen::VectorXd state(6);
+          state << 0, 0, 0, v, cte, epsi;
+          std::cout << "CTE: " << cte << std::endl;
+
+          // Run the MPC solver
+          auto vars = mpc.Solve(state, coeffs);
+
+          double steer_value = -vars[0] / deg2rad(25);
+          double throttle_value = vars[1];
 
           json msgJson;
           // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
@@ -107,15 +129,15 @@ int main() {
           msgJson["steering_angle"] = steer_value;
           msgJson["throttle"] = throttle_value;
 
-          //Display the MPC predicted trajectory 
-          vector<double> mpc_x_vals;
-          vector<double> mpc_y_vals;
+          //Display the MPC predicted trajectory
+          //vector<double> mpc_x_vals;
+          //vector<double> mpc_y_vals;
 
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Green line
 
-          msgJson["mpc_x"] = mpc_x_vals;
-          msgJson["mpc_y"] = mpc_y_vals;
+          msgJson["mpc_x"] = mpc.x_predict;
+          msgJson["mpc_y"] = mpc.y_predict;
 
           //Display the waypoints/reference line
           vector<double> next_x_vals;
@@ -123,6 +145,10 @@ int main() {
 
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Yellow line
+          for (int i = 0; i < ptsx.size(); i++) {
+            next_x_vals.push_back(ptsx_v[i]);
+            next_y_vals.push_back(ptsy_v[i]);
+          }
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
